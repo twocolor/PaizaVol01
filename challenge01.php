c205dca3258597c5f75cc40858460bd308bbde73
twocolor <twocolor.dev@gmail.com>
2014-05-26 04:06:40 +0900

    変数名とコメントを修正

--- challenge01.php
+++ challenge01.php
@@ -2,74 +2,70 @@
 // php 5.4.4
 
     /**
-     * 引数で与えた配列と最大値を利用して2分探索で最も高い配列添え字を返す
+     * 引数で与えた配列と目標値を利用して2分探索で最も高い配列添え字を返す
      * @param targetArr 探索対象の配列
-     * @param searchMax 最大値
-     * @param current
-     * @return 最も高い値の添字
+     * @param searchMax 目標値。これより値が低い中から最も値が高い配列添字を探す
+     * @param current 現在の配列添字。ここよりも値が高くなる添字を対象とする
+     * @return targetArrの添字
      */
-    function getMaxPoint($targetArr,$searchMax,$current = 0){
-        //既に比較してるものは候補に含めない
+    function getMaxIndex($targetArr, $searchMax, $current = 0){
+        // 探索対象の最小添字
         $low = $current;
-        $high = count($targetArr) -1;
-
+        $high = count($targetArr) - 1;
         if($targetArr[$high] <= $searchMax){
             return $high;
         }
-
         while (($high - $low) > 1) {
                 $mid = ceil(($low + $high) / 2);
                 if($targetArr[$mid] > $searchMax){
-                        //目標値が中央添え字より小さいなら、今確認した値を最大添え字にする
+                        //目標値が中央添え字より低いなら、今確認した値を最大添え字にする
                         $high = $mid;
                 } else {
-                        //目標値が中央添え字と同じか大きいなら、今確認した値を最小添え字にする
+                        //目標値が中央添え字と同じか高いなら、今確認した値を最小添え字にする
                         $low = $mid;
                 }
         }
         //同じ商品同士を比較した場合、一つ低い値を返す
         if($current == $low){
-            return $high -2;
+            return $high - 2;
         }
-        return $high-1;
+        return $high - 1;
     }
 
-
-    //標準入力を変数に格納
+    // 標準入力から今回のキャンペーンに関する情報を取得
     $input = trim(fgets(STDIN));
     $input = explode(" ", $input);
-    //商品数
+    // 全商品数
     $N = intval($input[0]);
-    //日数
+    // キャンペーン日数
     $D = intval($input[1]);
-    //商品価格
+    // 商品価格の配列
     $p = array();
     for ( $i = 0; $i < $N; $i++) {
         $input = trim(fgets(STDIN));
-        array_push($p,intval($input));
+        array_push($p, intval($input));
     }
     sort($p);
 
-    //目標価格
+    // 目標価格（$N, $D, $p, $mという変数名は問題に準拠してつけた）
     $m = array();
     for ($i = 0; $i < $D; $i++) {
-        $input = intval(trim(fgets(STDIN)));
+        // 目標金額の値をひとつずつ取得する
+        $m = intval(trim(fgets(STDIN)));
+        // 目標金額に最も近い組み合わせ金額
         $max = 0;
-        //目標金額の最大値の添え字
-        $lastIndex = getMaxPoint($p, $input);
-        for($j =0; $j < $lastIndex; $j++){
-            //目標金額より低い全商品の数だけ繰り返す
-            $target = $input - $p[$j];
-            //足し合わせて最大になる値を取得する
-            $tempMax = $p[$j] + $p[getMaxPoint($p, $target, $j)];
-
-            if($tempMax == $input){
-                $max = $tempMax;
+        // 目標金額に最も近い商品価格の添え字
+        $lastIndex = getMaxIndex($p, $m);
+        // 目標金額より低い全商品の数だけ繰り返す（処理回数を減らすため）
+        for($j =0; $j < $lastIndex; $j++) {
+            $target = $m - $p[$j];
+            $sum = $p[$j] + $p[getMaxIndex($p, $target, $j)];
+            if($sum == $m){
+                $max = $sum;
                 break;
             }
-
-            if($tempMax < $input && $tempMax > $max){
-                $max = $tempMax;
+            if($sum < $m && $sum > $max){
+                $max = $sum;
             }
         }
         echo $max . "\n";
